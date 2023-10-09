e46bcfcafad6bcef5aa93047fc400c2c59820051
exteriorduke319 <yarnevictanner@gmail.com>
2023-10-09 09:23:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double tempC;
+        double tempF;
+        boolean validInput = false;
+
+        do {
+            System.out.print("Please enter a temperature in Celsius to convert to Fahrenheit: ");
+            if (scan.hasNextDouble()) {
+                tempC = scan.nextDouble();
+                tempF = (tempC * 1.8) + 32;
+                System.out.printf("Degrees C: %12.2f", tempC);
+                System.out.printf("\nDegrees F: %12.2f", tempF);
+                validInput = true;
+            } else {
+                System.out.println("You have entered an incorrect value for temperature.  Please enter a number.");
+                scan.nextLine();
+            }
+        } while(!validInput);
+    }
+}
